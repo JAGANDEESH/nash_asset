b5e6baada8822f0cfae013ecbaf6e17bf7db1203
paulsudhan <paul11032003@gmail.com>
2024-03-14 16:27:04 +0530

    adding my doctypes

--- master/master/doctype/escalation_matrixs/test_escalation_matrixs.py
+++ master/master/doctype/escalation_matrixs/test_escalation_matrixs.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestCheckLists(FrappeTestCase):
+class TestEscalationMatrixs(FrappeTestCase):
 	pass
--- master/master/doctype/uom/uom.py
+++ master/master/doctype/uom/uom.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class CheckLists(Document):
+class UOM(Document):
 	pass
