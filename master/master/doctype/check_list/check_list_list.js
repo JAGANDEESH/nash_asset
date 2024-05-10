8829373b0819057e176f8267645f23640c6cce1e
paulsudhan <paul11032003@gmail.com>
2024-05-10 12:40:00 +0530

    sudhan pulls

--- master/master/doctype/check_list/check_list_list.js
+++ master/master/doctype/check_list/check_list_list.js
@@ -16,54 +16,54 @@
 //   },
 // };
 
-frappe.listview_settings["Check List"] = {
-  onload: function (listview) {
-    // Add your custom JavaScript code here
-    listview.page.add_inner_button(__("Add List"), function () {
-      // Display a dialog box to input new checklist data
-      var dialog = new frappe.ui.Dialog({
-        title: __("Add List"),
-        fields: [
-          {
-            label: __("Checklist Number"),
-            fieldname: "checklist_number",
-            fieldtype: "Data",
-            reqd: 1,
-          },
-          {
-            label: __("Checklist Description"),
-            fieldname: "checklist_description",
-            fieldtype: "Data",
-            reqd: 1,
-          },
-          {
-            label: __("M/C Types"),
-            fieldname: "machine_type_name",
-            fieldtype: "Data",
-            reqd: 1,
-          },
-        ],
-        primary_action: function () {
-          var values = dialog.get_values();
-          if (values) {
-            // Call a custom function to add the checklist
-            addChecklist(values, function (response) {
-              if (response.message && response.message.status === "success") {
-                frappe.show_alert(__("Values stored successfully"), 5);
-                var doc = frappe.model.sync(response.message)[0];
-                frappe.set_route("List", doc.doctype, doc.name);
-              } else {
-                frappe.msgprint(__("Error storing values"));
-              }
-            });
-            dialog.hide();
-          }
-        },
-      });
-      dialog.show();
-    });
-  },
-};
+// frappe.listview_settings["Check List"] = {
+//   onload: function (listview) {
+//     // Add your custom JavaScript code here
+//     listview.page.add_inner_button(__("Add List"), function () {
+//       // Display a dialog box to input new checklist data
+//       var dialog = new frappe.ui.Dialog({
+//         title: __("Add List"),
+//         fields: [
+//           {
+//             label: __("Checklist Number"),
+//             fieldname: "checklist_number",
+//             fieldtype: "Data",
+//             reqd: 1,
+//           },
+//           {
+//             label: __("Checklist Description"),
+//             fieldname: "checklist_description",
+//             fieldtype: "Data",
+//             reqd: 1,
+//           },
+//           {
+//             label: __("M/C Types"),
+//             fieldname: "machine_type_name",
+//             fieldtype: "Data",
+//             reqd: 1,
+//           },
+//         ],
+//         primary_action: function () {
+//           var values = dialog.get_values();
+//           if (values) {
+//             // Call a custom function to add the checklist
+//             addChecklist(values, function (response) {
+//               if (response.message && response.message.status === "success") {
+//                 frappe.show_alert(__("Values stored successfully"), 5);
+//                 var doc = frappe.model.sync(response.message)[0];
+//                 frappe.set_route("List", doc.doctype, doc.name);
+//               } else {
+//                 frappe.msgprint(__("Error storing values"));
+//               }
+//             });
+//             dialog.hide();
+//           }
+//         },
+//       });
+//       dialog.show();
+//     });
+//   },
+// };
 
 // // Function to add the checklist using AJAX call
 // function addChecklist(data, callback) {
