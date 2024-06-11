74e0d9ed3564bd913d388939c92fcadc3ba44f8d
paulsudhan <paul11032003@gmail.com>
2024-06-11 10:53:00 +0530

    sudhan pushing

--- master/preventive_maintenance/doctype/pm_work_order/pm_work_order_list.js
+++ master/preventive_maintenance/doctype/pm_work_order/pm_work_order_list.js
@@ -1,44 +1,57 @@
-frappe.listview_settings['PM WORK ORDER'] = {
-    onload: function(listview) {
-        listview.refresh();
-    },
-    onreload: function(listview) {
-        listview.page.sidebar.find('a:contains("Submitted")').text('Assigned');
-    },
-    get_indicator: function(doc) {
-        if (doc.work_order_status === 'Assigned') {
-            return [__('Assigned'), 'green', 'work_order_status,=,Assigned'];
-        } else if (doc.work_order_status === 'cancel') {
-            return [__('cancel'), 'red', 'work_order_status,=,cancel'];
-        }
-    },
-    
+frappe.listview_settings["PM WORK ORDER"] = {
+  onload: function (listview) {
+    listview.refresh();
+  },
 };
 
-frappe.ui.form.on('PM WORK ORDER', {
-    refresh: function(frm) {
-        // Add a custom button
-        frm.add_custom_button(__('Update'), function() {
-            frappe.call({
-                method: 'master.preventive_maintenance.doctype.pm_work_order.pm_work_order.update_status',  // Adjust the path to your update_status function
-                args: {
-                    docname: frm.doc.name,
-                    name : frm.doc.name,
-                    fieldname :{
-                        status : 'Inprogress',
-                        work_order_status : 'Inprogress'
-                    }
-                },
-                callback: function(response) {
-                    if (!response.exc) {
-                        frappe.msgprint(__('Status updated to InProgress'));
-                    } else {
-                        frappe.msgprint(__('Status updated to Assigned'));
-                    }
-                    frm.reload_doc(); 
-                }              
-                
-            });
-        });
-    }
+frappe.ui.form.on("PM WORK ORDER", {
+  refresh: function (frm) {
+    frm.add_custom_button(__("Assign"), function () {
+      frappe.call({
+        method:
+          "master.preventive_maintenance.doctype.pm_work_order.pm_work_order.update_status",
+        args: {
+          docname: frm.doc.name,
+          name: frm.doc.name,
+          fieldname: {
+            status: "Assigned",
+            work_order_status: "Assigned",
+          },
+        },
+        callback: function (response) {
+          console.log(response);
+          if (!response.exc) {
+            frappe.msgprint(__("Status updated to Assigned"));
+          } else {
+            frappe.msgprint(__("An error occurred while updating the status"));
+          }
+          frm.reload_doc();
+        },
+      });
+    });
+
+    frm.add_custom_button(__("Inprogress"), function () {
+      frappe.call({
+        method:
+          "master.preventive_maintenance.doctype.pm_work_order.pm_work_order.inprogress_status",
+        args: {
+          docname: frm.doc.name,
+          name: frm.doc.name,
+          fieldname: {
+            status: "Inprogress",
+            work_order_status: "Inprogress",
+          },
+        },
+        callback: function (response) {
+          console.log(response);
+          if (!response.exc) {
+            frappe.msgprint(__("Status updated to Inprogress"));
+          } else {
+            frappe.msgprint(__("An error occurred while updating the status"));
+          }
+          frm.reload_doc();
+        },
+      });
+    });
+  },
 });
