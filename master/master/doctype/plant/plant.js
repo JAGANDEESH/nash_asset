0976bb92a479c00e05a5d44d3f579fe241887276
Vishnuprak <amudharvichu93@gmail.com>
2024-06-11 17:21:49 +0530

    Department and masters

--- master/master/doctype/plant/plant.js
+++ master/master/doctype/plant/plant.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2024, mazework and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Plant", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Plant", {
+	onload: function(frm) {  
+        if(frm.doc.__islocal){
+            frm.set_value('created_date', frappe.datetime.now_datetime()); 
+            frm.set_value('created_by', frappe.session.user_fullname);  
+        }
+    } 
+});
